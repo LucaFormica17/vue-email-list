c25998673767bae1aeb9d5652ccfe5c611fe1d0a
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2023-12-20 17:04:47 +0100

    [UPDATE] bug-fixed

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ createApp({
     },
     methods:{
         generateEmail(){
+            this.emails = [];
             for(let i=0; i<10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>{
                 this.emails.push(response.data.response);
