9b5c23d4f89a9db509f652d01764e6e5390c9a27
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2023-12-20 16:54:00 +0100

    [UPDATE] layout and button with function

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ createApp({
         }
     },
     created() {
-        for(let i=0; i<10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>{
-            this.emails.push(response.data.response);
-            });
-        }
+        
     },
     methods:{
-        
+        generateEmail(){
+            for(let i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>{
+                this.emails.push(response.data.response);
+                });
+            }
+        }
     }
 }).mount('#app')
